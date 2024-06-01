541789b20081cb72f37ef372798839d793224b28
raffiMRG <mutekinoraffi@gmail.com>
2024-06-01 21:32:32 +0700

    doning UI

--- src/main/java/com/coba1gui/Coba1GUI.java
+++ src/main/java/com/coba1gui/Coba1GUI.java
@@ -15,7 +15,7 @@ public class Coba1GUI {
         // instansiasi jframe
         JFrame form = new JFrame("coba 1 gui cara manual");
         // atur ukuran frame
-        form.setSize(400, 300);
+        form.setSize(400, 550);
         // atur agar program berhenti ketika user menekan tombol close
         form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // atur agar ketika program dijalankan akan berada di tengah layar dan tidak relatif ke posisi 0,0
@@ -76,12 +76,84 @@ public class Coba1GUI {
         biayaPerhariText.setBounds(120, 155, 200, 25);
         form.add(biayaPerhariText);
         
+        // bagian tanggal pinjam
+        JLabel labelTanggalPinjam = new JLabel("Tanggal Pinjam");
+        labelTanggalPinjam.setBounds(20, 200, 100, 15);
+        form.add(labelTanggalPinjam);
         
+        String hari[] = new String[31];
+        for (int i = 0; i <hari.length; i++) {
+            hari[i] = Integer.toString(i+1);
+        }
         
+        JComboBox comboHari = new JComboBox(hari);
+        comboHari.setBounds(120, 195, 40, 25);
+        form.add(comboHari);
         
+        String bulan[] = {"Jan", "Feb", "Mar", "Apr", "Mei", "jun", "Jul", "Ags", "Sep", "Okt", "Nov", "Des"};
+        JComboBox comboBulan = new JComboBox(bulan);
+        comboBulan.setBounds(180, 195, 55, 25);
+        form.add(comboBulan);
         
+        String tahun[] = new String[15];
+        for (int i = 0; i <tahun.length; i++) {
+            tahun[i] = Integer.toString(i+2010);
+        }
+        JComboBox comboTahun = new JComboBox(tahun);
+        comboTahun.setBounds(250, 195, 70, 25);
+        form.add(comboTahun);
         
+        // bagian lama pinjam
+        JLabel labelLamaPinjam = new JLabel("Lama Pinjam");
+        labelLamaPinjam.setBounds(20, 240, 100, 15);
+        form.add(labelLamaPinjam);
         
+        String lamaPinjam[] = {"1", "2", "3", "4", "5", "6", "7"}; 
+        JComboBox comboLamaPinjam = new JComboBox(lamaPinjam);
+        comboLamaPinjam.setBounds(120, 235, 40, 25);
+        form.add(comboLamaPinjam);
+        
+        JLabel labelHariLamaPinjam = new JLabel("Hari");
+        labelHariLamaPinjam.setBounds(170, 235, 50, 25);
+        form.add(labelHariLamaPinjam);
+        
+        // bagian total biaya
+        JLabel totalBiaya = new JLabel("Total Biaya");
+        totalBiaya.setBounds(20, 280, 100, 15);
+        form.add(totalBiaya);
+        
+        JTextField totalBiayaText = new JTextField();
+        totalBiayaText.setBounds(120, 275, 200, 25);
+        form.add(totalBiayaText);
+        
+        // bagian Jumlah Bayar
+        JLabel JumlahBayar = new JLabel("Jumlah Bayar");
+        JumlahBayar.setBounds(20, 320, 100, 15);
+        form.add(JumlahBayar);
+        
+        JTextField JumlahBayarText = new JTextField();
+        JumlahBayarText.setBounds(120, 315, 200, 25);
+        form.add(JumlahBayarText);
+        
+        // bagian Kembalian
+        JLabel Kembalian = new JLabel("Kembalian");
+        Kembalian.setBounds(20, 360, 100, 15);
+        form.add(Kembalian);
+        
+        JTextField KembalianText = new JTextField();
+        KembalianText.setBounds(120, 355, 200, 25);
+        form.add(KembalianText);
+        
+        // bagian Simpan | Reset
+        JButton btnSimpan = new JButton("Simpan");
+        btnSimpan.setBounds(120, 395, 80, 25);
+        form.add(btnSimpan);
+        
+        JButton btnReset = new JButton("Reset");
+        btnReset.setBounds(240, 395, 80, 25);
+        form.add(btnReset);
+        
+        System.out.println("Done");
         
         form.setLayout(null);
         
